279fb4f653e5fb1bca00e0694005f7dc43d9960d
Alvaro Garcia <146588674+alvagarciia@users.noreply.github.com>
2025-04-22 16:33:52 -0500

    make script file class-friendly

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-// Initialize & declare Variables
-let city = "Fayetteville"
-let time = "13:03"
-let date = "Wednesday, 23 Apr"
+class WeatherInfo {
+    constructor(city, time, date, currentTemp, feelsTemp, sunrise, sunset, weather, humidity, wind, pressure, uv) {
+      this.city = city;
+      this.time = time;
+      this.date = date;
+      this.currentTemp = currentTemp;
+      this.feelsTemp = feelsTemp;
+      this.sunrise = sunrise;
+      this.sunset = sunset;
+      this.weather = weather;
+      this.humidity = humidity;
+      this.wind = wind;
+      this.pressure = pressure;
+      this.uv = uv;
+    }
 
-let currentTemp = 24
-let feelsTemp = 22
-let sunriseTime = "06:03"
-let sunsetTime = "06:53"
-let weather = "Sunny"
-let humidity = 40
-let windSpeed = 2
-let pressure = 997
-let uvIndex = 8 
+    updateDOM() {
+        document.getElementById("city-name").innerText = this.city;
+        document.getElementById("time").innerText = this.time;
+        document.getElementById("realTemp").innerText = `${this.currentTemp}°C`;
+        document.getElementById("feelTemp").innerText = `${this.feelsTemp}°C`;
+        document.getElementById("sunriseTime").innerText = `${this.sunriseTime} AM`;
+        document.getElementById("sunsetTime").innerText = `${this.sunsetTime} PM`;
+        document.getElementById("weather").innerHTML = this.weather;
+        document.getElementById("humidity").innerText = `${this.humidity} %`;
+        document.getElementById("windSpeed").innerText = `${this.windSpeed} km/h`;
+        document.getElementById("pressure").innerText = `${this.pressure} hPa`;
+        document.getElementById("uvIndex").innerText = this.uvIndex;
+    }
+}
 
-// Update HTML elements
-document.getElementById("city-name").innerText = city;
-document.getElementById("time").innerText = time;
-document.getElementById("date").innerText = date;
-
-document.getElementById("realTemp").innerText = `${currentTemp}°C`;
-document.getElementById("feelTemp").innerText = `${feelsTemp}°C`;
-document.getElementById("sunriseTime").innerText = `${sunriseTime} AM`;
-document.getElementById("sunsetTime").innerText = `${sunsetTime} PM`;
-document.getElementById("weather").innerHTML = weather;
-document.getElementById("humidity").innerText = `${humidity} %`;
-document.getElementById("windSpeed").innerText = `${windSpeed} km/h`;
-document.getElementById("pressure").innerText = `${pressure} hPa`;
-document.getElementById("uvIndex").innerText = uvIndex;
+const weather = new WeatherInfo("Fayetteville","13:03", "Wednesday, 23 Apr", 24, 22, "06:03", "06:53", "Sunny", 40, 2, 997, 8)
+weather.updateDOM()
 
